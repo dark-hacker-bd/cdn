315e4e16dfd841a7928e0fda71d54fcd86d3124b
dark boy(sorif) <94717681+dark-hacker-bd@users.noreply.github.com>
2024-08-14 23:48:30 -0700

    Add files via upload

---
+++ update.js
@@ -0,0 +1,32 @@
+        var link1 = "1.php";
+        var link2 = "2.php";
+        
+       
+
+        function openLink(url) {
+            window.open(url, '_blank');
+        }
+        
+        
+         $(document).ready(function(){
+
+        $('.b1') .click(function(){
+        $('.b11') .show(0, function(){
+        $('.b2') .click(function(){
+        $('.b22') .show(function(){
+        $('.b33') .show()
+                                })
+                          })
+                  })
+              });
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
